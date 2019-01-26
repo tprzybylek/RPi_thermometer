1fd6ad878ef76028d8d561f01a3a35f38e21590e
tomek <tajemniczyjogurt@gmail.com>
2019-01-26 17:10:21 +0100

    Add AQI data

--- thermo/admin.py
+++ thermo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Record
 
-# Register your models here.
+
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('date', 'temperature', 'humidity', 'outdoor_temperature', 'outdoor_humidity', 'aqi')
+
+
+admin.site.register(Record, RecordAdmin)
