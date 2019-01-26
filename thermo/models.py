e1b248f8fe313103e63922fb9b463ba9bb4ea604
tomek <tajemniczyjogurt@gmail.com>
2019-01-26 17:09:29 +0100

    add AQI fields

--- thermo/models.py
+++ thermo/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
+
 class Record(models.Model):
     date = models.fields.DateTimeField()
-    temperature = models.fields.DecimalField(max_digits=4, decimal_places=2)
-    humidity = models.fields.DecimalField(max_digits=4, decimal_places=2)
+    temperature = models.fields.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    humidity = models.fields.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    outdoor_temperature = models.fields.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    outdoor_humidity = models.fields.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    tvoc = models.fields.IntegerField(null=True, blank=True)
+    co2 = models.fields.IntegerField(null=True, blank=True)
+    aqi = models.fields.IntegerField(null=True, blank=True)
