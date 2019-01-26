e1406b4a548d0fa77797f7168fc9f8947293d356
tomek <tajemniczyjogurt@gmail.com>
2019-01-26 17:10:01 +0100

    save AQI data

--- thermo/management/commands/get_measurement.py
+++ thermo/management/commands/get_measurement.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from thermo.models import Record
-from thermo.views import get_current_reading
+from thermo.views import get_current_reading, get_air_quality
 from datetime import datetime
 
 
@@ -11,3 +11,14 @@ class Command(BaseCommand):
         reading = get_current_reading()
         r = Record(date=datetime.now(), temperature=reading['temperature'], humidity=reading['humidity'])
         r.save()
+
+        reading = get_air_quality()
+
+        if Record.objects.filter(date=reading['time'], aqi=reading['aqi']):
+            pass
+        else:
+            r = Record(date=reading['time'],
+                       outdoor_temperature=reading['outdoor_temperature'],
+                       outdoor_humidity=reading['outdoor_humidity'],
+                       aqi=reading['aqi'])
+            r.save()
